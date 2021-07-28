3ef29ed115981404238006215909bb5a3408f58f
konmarii <chankonworld@gmail.com>
2021-07-28 11:51:22 +0900

    繰り返し処理

--- app.js
+++ app.js
@@ -25,3 +25,22 @@ if(orange<apple) {
   alert('みかんの値段の方がりんごより高い')
 }
 
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num<max) {
+  num = num*2;
+  count = count+1;
+}
+
+alert('２をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let num = 0;
+
+for(i+1;i<11;i++) {
+  num = num + i;
+}
+
+alert('１から１０まで足した結果は' + num + 'です');
