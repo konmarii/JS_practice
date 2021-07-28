c56f9018f2e7c50b0083fbc424fcff6aaae024a8
konmarii <chankonworld@gmail.com>
2021-07-28 11:32:33 +0900

    [update]条件分岐

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 alert('Hello World');
+
+let hello = "Hello, World";
+alert(hello);
+
+alert(4+3);
+alert(8-5);
+alert(2*6);
+alert(10/2);
+
+alert("Hello"+"World");
+
+let str1 = 'Hello';
+let str2 = 'World!';
+alert(str1 + str2);
+
+let orange = 100;
+let apple = 120;
+
+if(orange<apple) {
+  alert('みかんの値段の方がりんごより安い')
+} else if(orange == apple) {
+  alert('みかんとりんごが同じ値段')
+} else {
+  alert('みかんの値段の方がりんごより高い')
+}
+
